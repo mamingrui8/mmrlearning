aa79933d2d7a8c8ed62355cf8fbbb446e5318966
mamingrui.gt <872068101@qq.com>
2019-01-09 18:05:18 +0800

    netty服务端编写。包括:1. 定义线程组 2. 初始化配置参数 3. 开启监听 4. 编写ChannelHandler处理器 netty客户端编写。包括:1. 定义线程组 2. 初始化配置参数 3. 发送请求至服务端 netty基础服务还剩下:netty客户端的处理器逻辑尚未编写。

---
+++ basicknowledge/src/main/java/com/mmr/io/netty/first/NettyClientHandler.java
@@ -0,0 +1,19 @@
+package com.mmr.io.netty.first;
+
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Description: 客户端处理器
+ * User: MaMingRui
+ * Email: mamr@broada.com
+ * Date: 2019年01月09日 16:59
+ * ModificationHistory: Who         When         What
+ * ---------  --------     ---------------------------
+ */
+public class NettyClientHandler extends ChannelInboundHandlerAdapter {
+    public void channelRead(ChannelHandlerContext ctx, Object msg){
+
+    }
+}
