18b48aa45b19c33266d5061de4e7c397a6d6daff
mamingrui.gt <872068101@qq.com>
2019-01-28 18:10:38 +0800

    集成disconf

--- basicknowledge/src/main/java/com/mmr/BasicKnowledgeApplication.java
+++ basicknowledge/src/main/java/com/mmr/BasicKnowledgeApplication.java
@@ -2,8 +2,10 @@ package com.mmr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource({"classpath:/config/disconf/disconf.xml"})
 public class BasicKnowledgeApplication {
 
     public static void main(String[] args) {
---
+++ basicknowledge/src/main/java/com/mmr/config/AppConfig.java
@@ -0,0 +1,20 @@
+package com.mmr.config;
+
+import com.baidu.disconf.client.common.annotations.DisconfFile;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Description: TODO
+ * User: MaMingRui
+ * Email: mamr@broada.com
+ * Date: 2019年01月28日 17:56
+ * ModificationHistory: Who         When         What
+ * ---------  --------     ---------------------------
+ */
+@Configuration
+@DisconfFile(filename = "/config/disconf/app.properties")
+public class AppConfig {
+    /**
+     *
+     */
+}
