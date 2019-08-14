920f869315be519e026a3e693377d15beb179eae
mamingrui8 <872068101@qq.com>
2019-08-14 23:41:24 +0800

    feat: 工厂模式与接口

---
+++ basicknowledge/src/main/java/com/mmr/learn/theory/thinkinginjava/practice18/CycleFactory.java
@@ -0,0 +1,66 @@
+package com.mmr.learn.theory.thinkinginjava.practice18;
+
+/**
+ * 工厂模式与接口
+ */
+public class CycleFactory {
+    public static void main(String[] args) {
+        Cycles.handle(new UnicycleFactory());
+        Cycles.handle(new BicycleFactory());
+        Cycles.handle(new TricycleFactory());
+    }
+}
+
+class Cycles{
+    public static void handle(Factory factory){
+        Cycle cycle = factory.getCycle();
+        cycle.ride();
+    }
+}
+
+interface Factory{
+    Cycle getCycle();
+}
+
+class UnicycleFactory implements Factory{
+    public Cycle getCycle(){
+        return new Unicycle();
+    }
+}
+
+class BicycleFactory implements Factory{
+    public Cycle getCycle(){
+        return new Bicycle();
+    }
+}
+
+class TricycleFactory implements Factory{
+    public Cycle getCycle(){
+        return new Tricycle();
+    }
+}
+
+interface Cycle{
+    void ride();
+}
+
+class Unicycle implements Cycle{
+    @Override
+    public void ride(){
+        System.out.println("Unicycle ride()...");
+    }
+}
+
+class Bicycle implements Cycle{
+    @Override
+    public void ride(){
+        System.out.println("Bicycle ride()...");
+    }
+}
+
+class Tricycle implements Cycle{
+    @Override
+    public void ride(){
+        System.out.println("Tricycle ride()...");
+    }
+}
